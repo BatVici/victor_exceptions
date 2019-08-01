27c8479a71fa5aab797e0090f775f620f95994e1
BatVici <viksen@abv.bg>
2019-08-01 13:29:17 +0300

    Add sumator exception

---
+++ src/main/java/com/clouway/exceptions/core/Program.java
@@ -0,0 +1,16 @@
+package com.clouway.exceptions.core;
+
+public class Program {
+    public static void main(String[] args) throws Sumator.UnsummableStringException {
+        Sumator sumator = new Sumator();
+
+        System.out.println(sumator.sum(5,6));
+        System.out.println(sumator.sum(5.7,6.7));
+        System.out.println(sumator.sum(55555555,4444444));
+        System.out.println(sumator.sum(44444444,55555555));
+
+        String st1 = "6";
+        String st2 = "7kj";
+        sumator.sum(st1,st2);
+    }
+}
